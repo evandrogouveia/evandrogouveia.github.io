0e57fc39037c6487169ccdcd69a98cc9ca7b61f9
Evandro da cruz gouveia <vando_gouveia@hotmail.com>
2017-11-04 22:42:18 -0200

    Add contato.php

---
+++ contato.php
@@ -0,0 +1,70 @@
+<?php
+                             //Variáveis
+                             $nome = $_POST['nome'];
+                             $email = $_POST['email'];
+                             $mensagem = $_POST['mensagem'];
+                             $data_envio = date('d/m/Y');
+                             $hora_envio = date('H:i:s');
+                             // Compo E-mail
+                             $arquivo = "
+                             <style type='text/css'>
+                              body {
+                              margin:0px;
+                              font-family:Verdane;
+                              font-size:12px;
+                              color: #666666;
+                             }
+                             a{
+                             color: #666666;
+                             text-decoration: none;
+                             }
+                             a:hover {
+                             color: #FF0000;
+                             text-decoration: none;
+                             }
+                             </style>
+                             <html>
+                                 <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+                                 <tr>
+                                 <td>
+                                 <tr>
+                                 <td width='500'><strong>Nome:</strong> $nome</td>
+                                 </tr>
+                                 <tr>
+                                 <td width='320'><strong>E-mail:</strong> <b>$email</b></td>
+                                 </tr>
+		                 <tr>
+                                 <td width='320' height='200' valign=top><strong>Mensagem:</strong><br/>$mensagem</td>
+                                 </tr>
+                                 </td>
+                                 </tr>  
+                                 <tr>
+                                 <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
+                                 </tr>
+                                 </table>
+                             </html>
+                             ";
+                              //enviar
+  
+                             // emails para quem será enviado o formulário
+                             $emailenviar = "vandogouveia67@gmail.com";
+                             $destino = $emailenviar;
+                             $assunto = "Contato enviado pelo Site";
+
+                             // É necessário indicar que o formato do e-mail é html
+                             $headers  = 'MIME-Version: 1.0' . "\r\n";
+                             $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                             $headers .= 'From: $nome <$email>';
+                             //$headers .= "Bcc: $EmailPadrao\r\n";
+                            
+                             $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+                             if($enviaremail){
+                             $mgm = "E-MAIL ENVIADO COM SUCESSO!";
+                             echo " <meta http-equiv='refresh' content='10;URL=index.php'>";
+                             } else {
+                             $mgm = "ERRO AO ENVIAR E-MAIL!";
+                             echo "$mgm";
+                             }
+                            
+                         ?>
+
